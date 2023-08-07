a2ba3eee4d712735a83329b1532c89659286139a
Şeyda Aydın <seydaydiin0@gmail.com>
2023-08-07 15:11:11 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Şeyda Aydın on 7.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
